684b7325cb388a29f8593e6c0a661ac499aaa68b
huent189 <hueahgase189@gmail.com>
2018-08-22 21:32:51 +0700

    update

--- Company.java
+++ Company.java
@@ -2,11 +2,36 @@ package JavaHRTechN3;
 import android.support.annotation.Nullable;
 
 public class Company extends Customer {
-    String name;
+    private String name;
     @Nullable
-    String phone;
+    private String phone;
     @Nullable String fax;
-    Contact mContact = new Contact();
-    Details billing = new Details();
-    Details shipping = new Details();
+    private Contact mContact = new Contact();
+    private Details billing = new Details();
+    private Details shipping = new Details();
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Nullable
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(@Nullable String phone) {
+        this.phone = phone;
+    }
+
+    @Nullable
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(@Nullable String fax) {
+        this.fax = fax;
+    }
 }
--- Customer.java
+++ Customer.java
@@ -1,10 +1,34 @@
 package JavaHRTechN3;
-import android.support.annotation.Nullable;
-
 public class Customer {
     public enum  CustomerType{
         Individual, Company;
     }
-    CustomerType type;
-    @Nullable String description;
+    private CustomerType type;
+    private String description;
+    private String CRM_ID;
+    public CustomerType getType() {
+        return type;
+    }
+
+    public void setType(CustomerType type) {
+        if(type != null){
+            this.type = type;
+        }
+        else {
+            System.out.println("Error");
+        }
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Customer(String CRM_ID, CustomerType type) {
+        this.type = type;
+        this.CRM_ID = CRM_ID;
+    }
 }
